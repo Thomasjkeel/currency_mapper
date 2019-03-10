3a1968c9f3f341e123118e3cd6eea31d54d208f5
Thomas Keel <thomasjames.keel@googlemail.com>
2019-03-10 16:49:56 +0000

    current updates to key files

--- getCurrencyFromApi.js
+++ getCurrencyFromApi.js
@@ -3,9 +3,18 @@ var currencyLayer;
 var access_key = '23e32a3cc70242915ea25f64d480fbb7';
 var endpoint = "latest";
 var allCurrencies; // stores the currency response
-// var baseCurrency = "GBP";
+var baseCurrency = "GBP";
+var lastCurrency = "EUR";
+var currentCurrency;
 
  function getCurrencies(baseCurrency) {
+     if (lastCurrency == baseCurrency){
+         console.log("Same as last currency so map is not updating")
+         return
+     } else {
+        lastCurrency = baseCurrency;
+     }
+
      client = new XMLHttpRequest();
      // make client request
      var url = 'http://data.fixer.io/api/' + endpoint + '?access_key=' + access_key + '&base=' + baseCurrency;
@@ -32,44 +41,36 @@ var allCurrencies; // stores the currency response
  }
 
  function updateCurrencieslayer(allCurrencies) {
+    countries[0].features.forEach(function (country) {
+        country.properties.value = NaN;
+    });
+
      for (var curr in allCurrencies.rates) {
         setCurrencyValue(curr, allCurrencies.rates[curr])
      }
+     if (countriesjs) {
+        countriesjs.remove();
+        console.log("Previous layer removed")
+    }
      countriesjs = new L.geoJson(countries, {
          onEachFeature: onEachFeature,
          style: style
-     }).addTo(mymap);
+        }).addTo(mymap);
      mymap.setMaxBounds(countriesjs.getBounds());
-}
+    }
 
  function setCurrencyValue(id, currencyValue) {
+    // loop through and update the values of the GeoJSON
+    var returned = false
     countries[0].features.forEach(function (country) {
         if (country.properties.currency == id) {
             country.properties.value = currencyValue;
-             console.log(country.properties)
-            return;
+            returned = true;
+            return true;
         }
     });
+    // give a default value
+    if (!returned) {
+        return false
+    }       
 }
-
-
-    //  for (var i = 0; i < countries[0].features.length; i++) {
-    //      if (countries[0].features[i].properties.ISO_A3 == id) {
-    //          countries[0].features[i].properties.value = currencyValue;
-    //         //  console.log(countries[0].features[i].properties.value, currencyValue)
-    //          return;
-    //      } else {
-    //          continue
-    //         //  console.log(countries[0].features[i].properties.ISO_A3)
-    //         //  console.log("No data" + id +  countries[0].features[i].properties.currency)
-    //      }
-    //  }
-
-    // countries = countries[0].features.properties.update(
-    //     list.findIndex(function (item) {
-    //         return item.get("currency") === id;
-    //     }),
-    //     function (item) {
-    //         return item.set("value", currencyValue);
-    //     }
-    // );
--- updateMap.js
+++ updateMap.js
@@ -1,14 +1,17 @@
-// function onEachFeature(feature, layer) {
-//     layer.setStyle({
-//         fillColor: 'red',
-//         fillOpacity: 0.5,
-//         weight: 1,
-//         color: 'darkred',
-//         opacity: 1
-//     });
-// }
+var info = L.control();
 
-var currentCurrency = "EUR";
+info.onAdd = function (map) {
+    this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
+    this.update();
+    return this._div;
+};
+
+// method that we will use to update the control based on feature properties passed
+info.update = function (props) {
+    this._div.innerHTML = '<h4>US Population Density</h4>' + (props ?
+        '<b>' + props.ADMIN + '</b><br />' + props.ADMIN + ' people / mi<sup>2</sup>' :
+        'Hover over a state');
+};
 
 function getColor(d) {
     return d > 1000 ? '#800026' :
@@ -18,19 +21,19 @@ function getColor(d) {
         d > 0.1 ? '#FD8D3C' :
         d > 0.01 ? '#FEB24C' :
         d > 0.001 ? '#FED976' :
-        '#FFEDA0';
+        '#d3d1c6';
 }
 
 function style(feature) {
     return {
         fillColor: getColor(feature.properties.value),
-        weight: 2,
+        weight: 1,
         opacity: 1,
-        // color: 'white',
-        dashArray: '3',
+        color: 'white',
         fillOpacity: 0.7
     };
 }
+
 function highlightFeature(e) {
     var layer = e.target;
 
@@ -46,17 +49,18 @@ function highlightFeature(e) {
     }
     info.update(layer.feature.properties.rates);
 }
+
 function resetHighlight(e) {
     countriesjs.resetStyle(e.target);
     info.update();
 }
 
 function zoomToFeature(e) {
-    mymap.fitBounds(e.target.getBounds());
+    // mymap.fitBounds(e.target.getBounds());
     // console.log(e.target.feature.properties);
     currentCurrency = e.target.feature.properties.currency;
     console.log(currentCurrency);
-    
+    getCurrencies(currentCurrency);
 }
 
 function onEachFeature(feature, layer) {
@@ -69,21 +73,23 @@ function onEachFeature(feature, layer) {
 
 
 
-var info = L.control();
+// var legend = L.control({
+//     position: 'bottomright'
+// });
 
-info.onAdd = function(map) {
-    this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
-    this.update();
-    return this._div;
-};
+// legend.onAdd = function (map) {
 
-// method that we will use to update the control based on feature properties passed
-info.update = function (props) {
-    this._div.innerHTML = '<h4>US Population Density</h4>' + (props ?
-        '<b>' + props.ADMIN + '</b><br />' + props.ADMIN + ' people / mi<sup>2</sup>' :
-        'Hover over a state');
-};
+//     var div = L.DomUtil.create('div', 'info legend'),
+//         grades = [0, 10, 20, 50, 100, 200, 500, 1000],
+//         labels = [];
 
+//     // loop through our density intervals and generate a label with a colored square for each interval
+//     for (var i = 0; i < grades.length; i++) {
+//         div.innerHTML +=
+//             '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+//             grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+//     }
 
-
+//     return div;
+// };
 
