48b37c4a21ee42e59a6671a03ade55a6e198a4f0
Thomas Keel <thomasjames.keel@googlemail.com>
2019-03-10 19:39:52 +0000

    add final versions of each of the key files

--- getCurrencyFromApi.js
+++ getCurrencyFromApi.js
@@ -1,15 +1,16 @@
-var client;
-var currencyLayer;
-var access_key = '23e32a3cc70242915ea25f64d480fbb7';
-var endpoint = "latest";
-var allCurrencies; // stores the currency response
+/* This File contains functions pertaining to getting the data from the API and loading the map layer*/
+
+var client; // stores the request to the API 
+var currencyLayer; // API response layer
+var access_key = '23e32a3cc70242915ea25f64d480fbb7'; // this is my private API -> the code will not work without this API key
+var allCurrencies; // stores the currency response from the API
 var baseCurrency = "GBP";
 var lastCurrency = "EUR";
 var currentCurrency = "GBP";
 
- function getCurrencies(baseCurrency) {
+function getCurrencies(baseCurrency) {
      if (lastCurrency == baseCurrency){
-         console.log("Same as last currency so map is not updating")
+         console.log("Selected Country uses the same currency as last currency so map is not updating")
          return
      } else {
         lastCurrency = baseCurrency;
@@ -17,48 +18,52 @@ var currentCurrency = "GBP";
 
      client = new XMLHttpRequest();
      // make client request
-     var url = 'http://data.fixer.io/api/' + endpoint + '?access_key=' + access_key + '&base=' + baseCurrency;
+     var url = 'http://data.fixer.io/api/latest' + '?access_key=' + access_key + '&base=' + baseCurrency;
      client.open('GET', url);
-     client.onreadystatechange = currenciesResponse; // earthquakeRepsonse is a method
+     client.onreadystatechange = currenciesResponse; // runs the response function
      client.send();
  };
 
- // create code to wait for response from the server and process it when recieved
- function currenciesResponse() {
+// create code to wait for response from the server and process it when recieved
+function currenciesResponse() {
      // listener
      if (client.readyState == 4) {
          var currenciesData = client.responseText;
+         console.log("loading GeoJSON layer")
          loadCurrencieslayer(currenciesData);
      }
  }
  
-  // convert the received data - which is text - to JSON format and add it to the map
- function loadCurrencieslayer(currenciesData) {
+// convert the received data - which is text - to JSON format and add it to the map
+function loadCurrencieslayer(currenciesData) {
      // parse the response to JSON
      allCurrencies = JSON.parse(currenciesData);
      updateCurrencieslayer(allCurrencies);
- }
+}
 
- function updateCurrencieslayer(allCurrencies) {
-    countries[0].features.forEach(function (country) {
-        country.properties.value = "Data Not Available";
-    });
-
-     for (var curr in allCurrencies.rates) {
-        setCurrencyValue(curr, allCurrencies.rates[curr])
-     }
-     if (countriesjs) {
-        countriesjs.remove();
-        console.log("Previous layer removed")
+function updateCurrencieslayer(allCurrencies) {
+countries[0].features.forEach(function (country) {
+    country.properties.value = "Data Not Available";
+});
+// loop through all the currencies and update the 'value' property on the GeoJSON layer for each country
+    for (var curr in allCurrencies.rates) {
+    setCurrencyValue(curr, allCurrencies.rates[curr])
     }
-     countriesjs = new L.geoJson(countries, {
-         onEachFeature: onEachFeature,
-         style: style
-        }).addTo(mymap);
-     mymap.setMaxBounds(countriesjs.getBounds());
-    }
+    // remove previous country GeoJSON layer
+    if (countriesjs) {
+    countriesjs.remove();
+    console.log("Previous layer removed")
+}
+// update current layer
+    countriesjs = new L.geoJson(countries, {
+        onEachFeature: onEachFeature,
+        style: style
+    }).addTo(mymap);
+    mymap.setMaxBounds(countriesjs.getBounds());
+}
 
- function setCurrencyValue(id, currencyValue) {
+// map the currency value from the API request to the GeoJSON (has the effect of updating the value property each of the countries)
+function setCurrencyValue(id, currencyValue) {
     // loop through and update the values of the GeoJSON
     countries[0].features.forEach(function (country) {
         if (country.properties.currency == id) {
--- updateMap.js
+++ updateMap.js
@@ -1,17 +1,9 @@
-function getColor(d) {
-    return d > 1000 ? '#800026' :
-        d > 100 ? '#BD0026' :
-        d > 10 ? '#E31A1C' :
-        d > 1 ? '#FC4E2A' :
-        d > 0.1 ? '#FD8D3C' :
-        d > 0.01 ? '#FEB24C' :
-        d > 0.001 ? '#FED976' :
-        '#d3d1c6';
-}
+/* This File contains functions pertaining to updating and styling of the map */
 
+/* STYLING OF GeoJSON layer. ADAPTED FROM: https: //leafletjs.com/examples/choropleth/*/
 function style(feature) {
     return {
-        fillColor: getColor(feature.properties.value),
+        fillColor: getColor(feature.properties.value), // gets the color based on the value of currency compared with base
         weight: 1,
         opacity: 1,
         color: 'white',
@@ -19,6 +11,34 @@ function style(feature) {
     };
 }
 
+// Function for mapping colors to the currency values returned from the API for each country
+function getColor(d) {
+    return d > 10000 ? '#540018' :
+        d > 1000 ? '#800026' :
+        d > 100 ? '#BD0026' :
+        d > 10 ? '#E31A1C' :
+        d > 5 ? '#FC4E2A' :
+        d > 2 ? '#FD8D3C' :
+        d > 1 ? '#FEB24C' :
+        d > 0.5 ? '#FED976' :
+        d > 0.1 ? '#ffe296' :
+        d > 0.01 ? '#ffefc4' :
+        d > 0.001 ? '#fff7e2' :
+        '#d3d1c6';
+}
+
+/* USER INTERACTION WITH GEOJSON LAYER. ADAPTED FROM: https: //leafletjs.com/examples/choropleth/ */
+// function that sets rules for user interaction with the GeoJSON layer
+function onEachFeature(feature, layer) {
+    layer.on({
+        mouseover: highlightFeature,
+        mouseout: resetHighlight,
+        click: setCurrency,
+        dblclick: zoomToFeature
+    });
+}
+
+// highlights a country that is hovered over
 function highlightFeature(e) {
     var layer = e.target;
 
@@ -37,9 +57,10 @@ function highlightFeature(e) {
 
 function resetHighlight(e) {
     countriesjs.resetStyle(e.target);
-    info.update();
+    info.update(); 
 }
 
+// sets the currency to pass to the API request (in getCurrencyFromApi.js) which will update the when a country has been clicked
 function setCurrency(e) {
     currentCurrency = e.target.feature.properties.currency;
     console.log(currentCurrency);
@@ -47,23 +68,20 @@ function setCurrency(e) {
     // highlightFeature(e);
 }
 
+// double clicking will zoom to the country that is clicked on
 function zoomToFeature(e) {
     mymap.fitBounds(e.target.getBounds());
 }
 
-function onEachFeature(feature, layer) {
-    layer.on({
-        mouseover: highlightFeature,
-        mouseout: resetHighlight,
-        click: setCurrency,
-        dblclick: zoomToFeature
-    });
-}
 
-
+/* LEGEND AND MENU. ADAPTED FROM: https: //leafletjs.com/examples/choropleth/ */
+// set two new controls for the legend and the hover-over menu on the page
 var info = L.control();
+var legend = L.control({
+    position: 'bottomright'
+});
 
-info.onAdd = function (map) {
+info.onAdd = function(map) {
     this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
     this.update();
     return this._div;
@@ -76,15 +94,10 @@ info.update = function (props) {
         'Hover over a country');
 };
 
-
-var legend = L.control({
-    position: 'bottomright'
-});
-
 legend.onAdd = function(map) {
 
     var div = L.DomUtil.create('div', 'info legend'),
-        grades = [NaN, 0.001, 0.01, 0.1, 1, 10, 100, 1000];
+        grades = [NaN, 0.001, 0.01, 0.1, 0.5, 1, 2, 5, 10, 100, 1000, 10000];
 
     div.innerHTML += '<b>Currency Value</b><br>'
     // loop through our density intervals and generate a label with a colored square for each interval
